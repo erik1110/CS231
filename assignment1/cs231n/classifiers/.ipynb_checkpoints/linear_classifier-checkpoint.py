f8da605d0530148d2be7d2e590d5454381fd85bb
Erik <oceanuheart@gmail.com>
2020-04-05 17:56:03 +0800

    finish SVM

--- assignment1/cs231n/classifiers/.ipynb_checkpoints/linear_classifier-checkpoint.py
+++ assignment1/cs231n/classifiers/.ipynb_checkpoints/linear_classifier-checkpoint.py
@@ -57,7 +57,9 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            index_sample = np.random.choice(num_train, size=batch_size)
+            X_batch = X[index_sample, :]
+            y_batch = y[index_sample]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -72,7 +74,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W += - learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -102,7 +104,8 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        score  = np.dot(X,self.W)
+        y_pred = np.argmax(score,axis=1) 
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
